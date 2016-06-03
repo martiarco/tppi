4733fdc19d03a21b5379c8d3640b49bdc89f6866
martiarco <martiarco1997@gmail.com>
2016-06-03 14:47:07 -0300

    Update BACKEND.c

--- BACKEND.c
+++ BACKEND.c
@@ -1,25 +1,40 @@
-BACK END
-# include <stdio.h>
-#define JUGADOR1 0
-#define JUGADOR2 1
-int cambiar_jugador(int x)
-{
-	if(x%2)
-		return JUGADOR1;
-	else
-		return JUGADOR2;
-}
-
 #include <stdio.h>
 #include "getnum.h"
 #include <stdlib.h>
 #include <math.h>
 #include "time.h"
 
+#define ERROR_MANCHA 100
+#define ERROR_TABLERO 101
+#define ERROR_DISTANCIA 102
+#define ERROR_ORIGEN 103
 #define BLOQUE 5
 #define ERROR 0
 #define BIEN 1
 #define EXIT 2
+typedef struct 
+{
+	int fil,col;
+} tipoPosicion;
+typedef struct 
+{
+	tipoPosicion origen,destino;
+} TipoMovimiento;
+typedef struct 
+{
+	char id;
+	int cantidadBlobs;
+} TipoJugador;
+typedef struct
+{
+	char** matriz;
+	int filas,columnas;
+} tipoTablero;
+typedef TipoJugador TipoJugadores[2];
+#define FUERA_TABLERO(x,y,fil,col) (x<0 || x>fil || y<0 || y>col)
+#define MAX(x,y) (x > y? x : y)
+#define ELEGIR_JUGADOR(x) (x == 0? 'A' : 'Z')
+
 //falta
 int imprimirMenu(void)
 {
@@ -37,35 +52,47 @@ int imprimirMenu(void)
 
 void seleccionarOpcion(int opcion)
 {
-
-	switch opcion
-	case 1:
-	paraJugar();
-	
-	case 2:
-	paraJugar();
-
-	case 3:
-	//TO DO 
-	//load
-	case 4:
-	exit(0);
-
-	
-
+	switch (opcion)
+	{
+		case 1:
+			paraJugar();
+		break;
+		case 2:
+			paraJugar();
+		break;
+		case 3:
+			//TO DO 
+			//load
+		break;
+		case 4:
+			exit(0);
+		break;
+	}
 }
 
-int paraJugar(opcion)
+void paraJugar()
 {
 	tipoTablero tablero;
+	tipoJugadores jugadores;
+	int i = 0;
+	jugadores[0].id = ELEGIR_JUGADOR(randInt(0,1));
+	if(jugadores[0].id == 'A')
+		jugadores[1].id = 'Z';
+	else
+		jugadores[1].id = 'A';
+	printf("Comienza el jugador 1 (%c)", jugadores[0].id);
 	tablero.matriz = leerDimension();
 	while(movPosibles(&tablero, jugador.id) != 0)
 	{ 
-	imprimirTablero(&tablero);
-	//quejugador e imprime quejugador
-	leerEntrada();
+		imprimirTablero(&tablero);
+		printf("Turno del jugador %c\n", jugadores[i].id);
+		//quejugador e imprime quejugador
+		leerEntrada();
+		if(i == 1)
+			i=0;
+		else
+			i=1;
 	}
-
 }
 
 char** leerDimension()
@@ -82,7 +109,126 @@ char** leerDimension()
 	return tablero.matriz; 
 }
 
+int randInt(int a, int b)
+{
+    return ((rand() / ((double)RAND_MAX + 1)) * (b - a + 1)) + a;
+} 
 
+void imprimirTablero(tipoTablero*tablero)
+{
+	int i, j;
+	for(i=0; i<tablero->filas; i++)
+	{
+		for(j=0; j<tablero->columnas; j++)
+			printf("%c ", tablero->matriz[i][j]);
+		putchar('\n');
+	}
+	putchar('\n');
+}
+
+char** generaMatriz(int fil,int col)
+{
+	char**p, i;
+	if(p=malloc(fil*sizeof(int)))
+	{
+		for(i=0;i<col;i++)
+		{
+			if(!(p[i]=malloc(col*sizeof(char))));
+				return NULL;
+		}
+	p[0][0] = 'A';
+	p[fil-1][0] = 'A';
+	p[0][col-1] = 'Z';
+	p[fil-1][col-1] = 'Z';
+	}
+	return p;
+}
+int esPosible(tipoTablero*tablero, tipoPosicion celda)
+{
+	int i, j;
+	for(i=celda.fil-2; i<=celda.fil+2; i++)
+	{
+		if(i>-1 && i<tablero->filas)
+		{									 //recibe el lugar donde hay un blob y revisa
+			for(j=celda.col-2; j<=celda.col+2; j++)
+			{
+				if(j>-1 && j<tablero->columnas)
+				{											 // si hay un vacio a distancia 2 o menos
+					if(tablero->matriz[i][j]=='0')
+						return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+int movPosibles(tipoTablero*tablero, char jugador)
+{ 
+	int flag=0, i, j;    //devuelve 1 si hay movimientos posibles para el jugador
+	tipoPosicion celda;
+	for(celda.fil=0; celda.fil<tablero->filas && !flag; celda.fil++)
+	{
+		for(celda.col=0; celda.col<tablero->columnas && !flag; celda.col++)
+		{
+			if(tablero->matriz[celda.fil][celda.col] == jugador)
+				flag = esPosible(tablero,celda);
+		}
+	}
+	return flag;
+}
+
+//devuelve cuantas manchas come del otro jugador
+int comeMancha(tipoTablero*tablero, tipoPosicion celda, char jugador)
+{
+	int i, j, cant = 0;
+	for(i=celda.fil-1; i<=celda.fil+1; i++)
+	{
+		if(i>-1 && i<tablero->filas)
+		{
+			for(j=celda.col-1; j<=celda.col+1; j++)
+			{	
+				if(j>-1 && j<tablero->columnas)
+				{
+					if(tablero->matriz[i][j] != jugador && tablero->matriz[i][j] != '0')
+					{
+						tablero->matriz[i][j] = jugador;
+						cant++;
+					}
+				}
+			}
+		}
+	}
+	return cant;
+}
+
+// devuelve cantidad de blobs que aumenta el jugador en una jugada
+int jugada(tipoTablero*tablero, TipoMovimiento*celdas, char jugador, int distancia)
+{
+	int cant = 0;
+	tablero->matriz[celdas->destino.fil][celdas->destino.col] = jugador;
+	if(distancia == 2)
+		tablero->matriz[celdas->origen.fil][celdas->origen.col] = 0;
+	else
+		cant++;
+	cant += comeMancha(tablero,celdas->destino,  jugador);
+	return cant;
+}
+
+//devuelve 0 si hay un error en el movimiento, sino devuelve el tipo de error
+int validarMovimiento(tipoTablero*tablero, TipoMovimiento*celdas,  char jugador, int distancia)
+{
+	if (tablero->matriz[celdas->destino.fil][celdas->destino.col] > 0)
+		return ERROR_MANCHA;
+	else if(FUERA_TABLERO(celdas->origen.fil, celdas->origen.col,tablero->filas,tablero->columnas) || FUERA_TABLERO(celdas->destino.fil, celdas->destino.col, tablero->filas, tablero->columnas))
+		return ERROR_TABLERO;
+	else if (distancia > 2 || distancia == 0)
+		return ERROR_DISTANCIA;
+	else if (tablero->matriz[celdas->origen.fil][celdas->origen.col] != jugador)
+		return ERROR_ORIGEN;
+	else
+		return 0;
+}
 
 int decidirEntrada(char * entrada, tipoJugador * jugador)
 {
